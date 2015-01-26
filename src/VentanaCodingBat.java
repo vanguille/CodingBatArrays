df5e98d19162de4079d0fb0adc442c48e3935392
Guillermo F <Guillermo F@Guillermo>
2015-01-26 10:56:12 +0100

    Este es el TERCER comentario de ejercicio Start 1 codingBat

--- src/VentanaCodingBat.java
+++ src/VentanaCodingBat.java
@@ -9,9 +9,23 @@
  * @author xp
  */
 public class VentanaCodingBat extends javax.swing.JFrame {
-
+  //--------------------------TESTEADOR-----------------------
+    
+    //DESCOMENTAR Y COMENTAR LAS POSIBILIDADES DE PROBLEMAS PARA TEST
+    
     int a [] = {1, 3, 4};
     int b [] = {5, 3, 4};
+    //int a [] = {1, 3, 4};
+    //int b [] = {1, 3, 4};
+    //int a [] = {};
+    //int b [] = {};
+    //int a [] = {};
+    //int b [] = {5};
+    //int a [] = {1, 3, 4};
+    //int b [] = {};
+    
+  //----------------------------------------------------------  
+    
     int n = 0;
     public VentanaCodingBat() {
         initComponents();
@@ -24,22 +38,27 @@ public class VentanaCodingBat extends javax.swing.JFrame {
          //-----------------------------------
  if (a.length == 0 && b.length == 0){
      return n;}  
-
     if (a.length == 0 && b.length >= 1){
+        
     if( b[0] == 1){
-      return 1;}
+        n = n+1;
+      return n;}
+    
       if( b[0] != 1){
       return n;}
     }
-        
+    
+    
     if (a.length >= 1 && b.length == 0){
     if( a[0] == 1 ){
         n = n+1;
-      return n;     
-    }   
+      return n;  }   
+    
        if( a[0] != 1){
       return 0;}
     }
+    
+    
   if ( a[0] == 1 && b[0] == 1 ){ 
       n = n+2;
         return n;
@@ -56,30 +75,23 @@ public class VentanaCodingBat extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Pulsa si tienes Eggs");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 jButton1MousePressed(evt);
             }
         });
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("jButton2");
-
-        jButton3.setText("jButton3");
-
-        jLabel1.setText("jLabel1");
-
-        jLabel2.setText("jLabel2");
-
-        jLabel3.setText("jLabel3");
+        jLabel1.setText("--Resultado--");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -87,37 +99,19 @@ public class VentanaCodingBat extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(227, Short.MAX_VALUE))
+                .addComponent(jButton1)
+                .addGap(27, 27, 27)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(127, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
+                .addGap(127, 127, 127)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jLabel1))
-                .addGap(78, 78, 78)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3)
-                    .addComponent(jLabel3))
-                .addGap(47, 47, 47))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(92, Short.MAX_VALUE))
         );
 
         pack();
@@ -127,6 +121,10 @@ public class VentanaCodingBat extends javax.swing.JFrame {
         start1(a, b);
         jLabel1.setText(String.valueOf(n));
     }//GEN-LAST:event_jButton1MousePressed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,10 +163,6 @@ public class VentanaCodingBat extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
